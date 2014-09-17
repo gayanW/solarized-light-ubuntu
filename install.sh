e3972e0b79746cf6de85f90181b89c0bf97a4137
Gayan Weerakutti <reversiblean@live.com>
2014-09-17 14:04:56 +0530

    install.sh: installtion script  "Simple bash script to setup solarized light colors for GNU ls and to change the colors of gnome terminal"

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# setup solarized light for GNU ls
+cp --verbose --force ./dircolors-solarized/dircolors.ansi-light ~/.dircolors
+eval `dircolors ~/.dircolors`
+
+# setup solarized light color scheme for gnome terminal
+./gnome-terminal-colors-solarized/install.sh
+
+echo "You must restart your gnome terminal for the changes to take effect."
